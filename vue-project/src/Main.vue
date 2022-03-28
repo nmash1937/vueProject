37a0677cda3b236c8d2422afab90a11f1249eb01
Nick Masciandaro <nmash1937@icloud.com>
2022-03-27 21:55:04 -0400

    Update toolbar

--- vue-project/src/Main.vue
+++ vue-project/src/Main.vue
@@ -1,29 +1,34 @@
 <template>
 <div >
     <div class="router-container">
-            <router-link to='/'>Home</router-link>
-            <router-link to='/about'>About</router-link>
+        <b-link  v-bind:to="'/'" router-tag="b-button">Home</b-link>
+        <b-link  v-bind:to="'/about'" router-tag="b-button">About</b-link>
     </div>
-
-
-    <router-view></router-view>
+<router-view></router-view>
 </div>
 
 </template>
 
 <style scoped>
 
+.btn.btn-secondary {
+    margin: 0.5em
+}
 .router-container {
     margin-left: auto;
     margin-right: auto;
     width: fit-content;
     margin-top: 1em;
 }
+a:hover {
+    color: white;
+}
  a {
      padding: 0.5em;
      margin: 0.5em;
      min-width: 100px;
-     background-color:aqua;
+     background-color:rgb(0, 153, 255);
+     color: white;
      border-radius: .5em;
  }
 </style>
